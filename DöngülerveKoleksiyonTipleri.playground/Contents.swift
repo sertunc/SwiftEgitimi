71810cf386083227d8963f32ca78c2f41d48fcfc
Sertunc SELEN <sertuncselen@Miray-MacBook-Air.local>
2019-10-28 01:42:29 +0300

    ikinci ders güncelleme

--- DöngülerveKoleksiyonTipleri.playground/Contents.swift
+++ DöngülerveKoleksiyonTipleri.playground/Contents.swift
@@ -1,3 +1,5 @@
+//Döngüler
+//For Döngüsü
 for index in 1...5 {
     print(index)
 }
@@ -13,8 +15,42 @@ for sayi in stride(from: 0, through: 60, by: aralik_2) {
     print(sayi)
 }
 
-var number = Int.random(in: 0 ..< 10)
+//While Döngüsü
+func GenerateRandomInt()->Int{
+    return Int.random(in: 0 ..< 10)
+}
+
+var number = GenerateRandomInt()
 while number < 7 {
     print("random: \(number)")
-    number = Int.random(in: 0 ..< 10)
+    number = GenerateRandomInt()
+}
+
+//Diziler
+var isimler=["ali","ayse","fatma"]
+print(isimler[0])
+isimler.append("veli")
+isimler.count
+isimler.insert("sertunc", at: 0)
+isimler.remove(at: 0)
+isimler
+
+for item in isimler {
+    print(item)
+}
+
+//Setler. Sırasız bulunurlar ve aynı değerden birden fazla eklenemez
+var tcler:Set = [123,654,348]
+tcler.insert(444)
+print(tcler)
+
+//Dictionary
+var liste=[121:"sertunc",178:"selen"]
+liste.removeValue(forKey: 121)
+liste.count
+liste.isEmpty
+liste[178]="selenn"
+
+for (tc,isim) in liste {
+    print("tc: \(tc) isim: \(isim)")
 }
